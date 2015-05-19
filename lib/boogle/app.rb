d9d9bd1113c0fd68dff325077913c13d5a75dcc2
Jared Stemen <jstemen@gmail.com>
2015-05-19 09:07:36 -0700

    adding mising files

---
+++ lib/boogle/app.rb
@@ -0,0 +1,66 @@
+require 'sinatra'
+require 'json'
+
+
+# word => [page_id, page_id]
+WORD_MAP = {}
+
+=begin
+# Endpoint to handle searching
+GET /search?query=Elementary,%20dear%20Watson
+- Request
+Accept: application/json
+
+- Response
+HTTP/1.1 200 OK
+Content-Type: application/json
+
+{
+“matches”: [
+    {
+“pageId”: 300,
+    “score”: 3
+},
+    {
+“pageId”: 12,
+    “score”: 1
+}
+]
+}
+=end
+get '/search' do
+  query = params['query']
+  page_ids = Set.new
+  query.split(' ').each { |q_word|
+    WORD_MAP[q_word].each { |page_id|
+      page_ids << page_id
+    }
+  }
+  matches = {}
+  page_ids.each { |page_id|
+    matches[:pageId] = page_id
+    matches[:score] =1
+  }
+  my_bod = {matches: matches}.to_json
+  [200, {'Content-Type' => 'application/json'}, my_bod]
+end
+
+
+=begin
+Content-Type: application/json
+Accept: application/json
+
+{
+“pageId”: 300,
+    “content”: “Elementary, dear Watson”
+}
+=end
+post '/index' do
+  page_id = params['pageId']
+  content = params['content']
+  content.split(' ').each { |word|
+    WORD_MAP[word] ||= []
+    WORD_MAP[word] << page_id
+  }
+
+end
---
+++ test/boogle/test_app.rb
@@ -0,0 +1,32 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../../lib/boogle/app'
+require 'test/unit'
+require 'rack/test'
+
+
+
+class AppTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_it_can_consume_and_respond
+    body = {
+        pageId: 300,
+        content: "Elementary, dear Watson"
+    }
+
+    post '/index', body
+    assert last_response.ok?
+
+    get '/search', {query: 'dear Watson'}
+    assert last_response.ok?
+    res = { matches: {pageId: '300',score:  2} }.to_json
+    assert_equal res, last_response.body
+
+  end
+
+end
