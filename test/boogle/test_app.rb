eeaf0e87021f6b9b0a2d806f2da37bd5e4489256
Jared Stemen <jstemen@gmail.com>
2015-05-19 09:14:46 -0700

    stubbing out tests

--- test/boogle/test_app.rb
+++ test/boogle/test_app.rb
@@ -5,7 +5,6 @@ require 'test/unit'
 require 'rack/test'
 
 
-
 class AppTest < Test::Unit::TestCase
   include Rack::Test::Methods
 
@@ -13,20 +12,48 @@ class AppTest < Test::Unit::TestCase
     Sinatra::Application
   end
 
-  def test_it_can_consume_and_respond
-    body = {
-        pageId: 300,
-        content: "Elementary, dear Watson"
-    }
 
+  def query_index(query)
+    get '/search', {query: query}
+    assert last_response.ok?
+  end
+
+  def load_index_with_book(page_content, page_id=300)
+    body = {
+        pageId: page_id,
+        content: page_content
+    }
     post '/index', body
     assert last_response.ok?
+  end
 
-    get '/search', {query: 'dear Watson'}
-    assert last_response.ok?
-    res = { matches: {pageId: '300',score:  2} }.to_json
+  def test_it_can_consume_and_respond
+    load_index_with_book("Elementary, dear Watson")
+
+    query_index('dear Watson')
+    res = {matches: {pageId: '300', score: 2}}.to_json
     assert_equal res, last_response.body
 
+  end
+
+  def test_querries_are_case_insenitive
+    skip "Implement me"
+  end
+
+  def test_that_puncutation_is_ignored
+    skip "Implement me"
+  end
+
+  def test_that_querries_disreguard_order
+    skip "Implement me"
+  end
+
+  def test_that_querries_disreguard_word_frequency
+    skip "Implement me"
+  end
+
+  def test_that_partial_matches_work
+    skip "Implement me"
   end
 
 end
